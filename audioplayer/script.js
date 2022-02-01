b9c8941edcaf7787d4197c08e6d2e297e6ccbd9e
Iván Mayol Braza <92253706+MayolDev@users.noreply.github.com>
2022-02-01 21:09:22 +0100

    subo audio

---
+++ audioplayer/script.js
@@ -0,0 +1,54 @@
+const playerButton = document.querySelector('.player-button'),
+      audio = document.querySelector('audio'),
+      timeline = document.querySelector('.timeline'),
+      soundButton = document.querySelector('.sound-button'),
+      playIcon = `
+        <image src="./image/boton-de-play.png"/>
+      `,
+      pauseIcon = `
+      <image src="./image/pause.png"/>
+      `,
+      soundIcon = `
+      <image src="./image/volumen.png"/>`,
+      muteIcon = `
+      <image src="./image/silent.png"/>`;
+
+function toggleAudio () {
+  if (audio.paused) {
+    audio.play();
+    playerButton.innerHTML = pauseIcon;
+  } else {
+    audio.pause();
+    playerButton.innerHTML = playIcon;
+  }
+}
+
+playerButton.addEventListener('click', toggleAudio);
+
+function changeTimelinePosition () {
+  const percentagePosition = (100*audio.currentTime) / audio.duration;
+  timeline.style.backgroundSize = `${percentagePosition}% 100%`;
+  timeline.value = percentagePosition;
+}
+
+audio.ontimeupdate = changeTimelinePosition;
+
+function audioEnded () {
+  playerButton.innerHTML = playIcon;
+}
+
+audio.onended = audioEnded;
+
+function changeSeek () {
+  const time = (timeline.value * audio.duration) / 100;
+  audio.currentTime = time;
+}
+
+timeline.addEventListener('change', changeSeek);
+
+function toggleSound () {
+  audio.muted = !audio.muted;
+  soundButton.innerHTML = audio.muted ? muteIcon : soundIcon;
+}
+
+soundButton.addEventListener('click', toggleSound);
